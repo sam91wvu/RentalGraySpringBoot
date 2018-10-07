c2a5cdfcb75100cb96bd685a3c5410deaa8a2d12
Samantha Brooks <sbrooks9@mix.wvu.edu>
2018-10-07 15:20:52 -0400

    end points now work

--- src/main/java/com/example/demo/controllers/SearchController.java
+++ src/main/java/com/example/demo/controllers/SearchController.java
@@ -15,30 +15,38 @@ public class SearchController {
     @RequestMapping("/search/**")
     @ResponseBody
     public List returnItems(@RequestParam(value="item", defaultValue="all") String name) {
-        List itemList = new ArrayList<Item>();
+        ArrayList<Item> itemList = new ArrayList<Item>();
+        ArrayList<Item>  filteredList =  new ArrayList<Item>();
 
-        itemList.add(new Item(1,"Table", "assets/images/table.jpeg", "This is a table picture."));
-        itemList.add(new Item(2,"Chair", "assets/images/chair.jpeg", "This is a chair"));
-        itemList.add(new Item(3,"Tent", "assets/images/tent.jpeg", ""));
-        itemList.add(new Item(4,"Cup", "assets/images/cup.jpeg",""));
-        itemList.add(new Item(6,"Bowl", "assets/images/bowl.jpeg", ""));
-        itemList.add(new Item(7,"Table", "assets/images/table.jpeg", "This is a table picture."));
-        itemList.add(new Item(8,"Chair", "assets/images/chair.jpeg", "This is a chair"));
-        itemList.add(new Item(9,"Tent", "assets/images/tent.jpeg", ""));
-        itemList.add(new Item(10,"Cup", "assets/images/cup.jpeg",""));
-        itemList.add(new Item(11,"Bowl", "assets/images/bowl.jpeg", ""));
-        itemList.add(new Item(12,"Table", "assets/images/table.jpeg", "This is a table picture."));
-        itemList.add(new Item(13,"Chair", "assets/images/chair.jpeg", "This is a chair"));
-        itemList.add(new Item(14,"Tent", "assets/images/tent.jpeg", ""));
-        itemList.add(new Item(15,"Cup", "assets/images/cup.jpeg",""));
-        itemList.add(new Item(16,"Bowl", "assets/images/bowl.jpeg", ""));
-        itemList.add(new Item(17,"Table", "assets/images/table.jpeg", "This is a table picture."));
-        itemList.add(new Item(18,"Chair", "assets/images/chair.jpeg", "This is a chair"));
-        itemList.add(new Item(19,"Tent", "assets/images/tent.jpeg", ""));
-        itemList.add(new Item(20,"Cup", "assets/images/cup.jpeg",""));
-        itemList.add(new Item(21,"Bowl", "assets/images/bowl.jpeg", ""));
-        itemList.add(new Item(22,"Bowl", "assets/images/bowl.jpeg", ""));
+        itemList.add(new Item(1,"White Table", "assets/images/table.jpeg", "This is a table picture."));
+        itemList.add(new Item(2,"White Chair", "assets/images/chair.jpeg", "This is a chair"));
+        itemList.add(new Item(3,"Yellow Tent", "assets/images/tent.jpeg", ""));
+        itemList.add(new Item(4,"White Cup", "assets/images/cup.jpeg",""));
+        itemList.add(new Item(6,"Blue Bowl", "assets/images/bowl.jpeg", ""));
+        itemList.add(new Item(7,"Marble Table", "assets/images/table.jpeg", "This is a table picture."));
+        itemList.add(new Item(8,"Desk Chair", "assets/images/chair.jpeg", "This is a chair"));
+        itemList.add(new Item(9,"Outside Tent", "assets/images/tent.jpeg", ""));
+        itemList.add(new Item(10,"Coffee Cup", "assets/images/cup.jpeg",""));
+        itemList.add(new Item(11,"Breakfast Bowl", "assets/images/bowl.jpeg", ""));
+        itemList.add(new Item(12,"Kitchen Table", "assets/images/table.jpeg", "This is a table picture."));
+        itemList.add(new Item(13,"Office Chair", "assets/images/chair.jpeg", "This is a chair"));
+        itemList.add(new Item(14,"Four Person Tent", "assets/images/tent.jpeg", ""));
+        itemList.add(new Item(15,"Handled Cup", "assets/images/cup.jpeg",""));
+        itemList.add(new Item(16,"Small Bowl", "assets/images/bowl.jpeg", ""));
+        itemList.add(new Item(17,"Large Table", "assets/images/table.jpeg", "This is a table picture."));
+        itemList.add(new Item(18,"Single Chair", "assets/images/chair.jpeg", "This is a chair"));
+        itemList.add(new Item(19,"Large Tent", "assets/images/tent.jpeg", ""));
+        itemList.add(new Item(20,"Medium Cup", "assets/images/cup.jpeg",""));
+        itemList.add(new Item(21,"Single Bowl", "assets/images/bowl.jpeg", ""));
+        itemList.add(new Item(22,"Large Blue Bowl", "assets/images/bowl.jpeg", ""));
 
-        return itemList;
+
+        for(int i=0; i< itemList.size(); i++){
+            Item item = itemList.get(i);
+            if(item.getTitle().contains(name)){
+                filteredList.add(item);
+            }
+        }
+        return filteredList;
     }
 }
